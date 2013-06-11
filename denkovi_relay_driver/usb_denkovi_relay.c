f35df2ce569f11a8fef30de70f01323f9052f0a8
Vinicius Tinti <viniciustinti@gmail.com>
2013-06-10 21:15:08 -0300

    Remove input logic
    Signed-off-by: Vinicius Tinti <viniciustinti@gmail.com>

--- denkovi_relay_driver/usb_denkovi_relay.c
+++ denkovi_relay_driver/usb_denkovi_relay.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2013 Vinicius Tinti (viniciustinti@gmail.com)
- * 
+ *
  * Copyright (C) 2001-2004 Greg Kroah-Hartman (greg@kroah.com)
  *
  *	This program is free software; you can redistribute it and/or
@@ -24,11 +24,9 @@
 #include <linux/usb.h>
 #include <linux/mutex.h>
 
-/* Define these values to match your devices */
 #define USB_DKVR_VENDOR_ID	0x0403
 #define USB_DKVR_PRODUCT_ID	0x6001
 
-/* table of devices that work with this driver */
 static const struct usb_device_id dkvr_table[] = {
 	{ USB_DEVICE(USB_DKVR_VENDOR_ID, USB_DKVR_PRODUCT_ID) },
 	{ }					/* Terminating entry */
@@ -37,33 +35,21 @@ MODULE_DEVICE_TABLE(usb, dkvr_table);
 
 /* Get a minor range for your devices from the usb maintainer */
 #define USB_DKVR_MINOR_BASE	192
-
-/* our private defines. if this grows any larger, use your own .h file */
 #define MAX_TRANSFER		(PAGE_SIZE - 512)
-/* MAX_TRANSFER is chosen so that the VM is not stressed by
-   allocations > PAGE_SIZE and the number of packets in a page
-   is an integer 512 is the largest possible packet on EHCI */
 #define WRITES_IN_FLIGHT	8
-/* arbitrarily chosen */
 
 struct usb_dkvr {
-	struct usb_device		*udev;					/* the usb device for this device */
-	struct usb_interface	*interface;				/* the interface for this device */
-	struct semaphore		limit_sem;				/* limiting the number of writes in progress */
-	struct usb_anchor		submitted;				/* in case we need to retract our submissions */
-	struct urb				*bulk_in_urb;			/* the urb to read data with */
-	unsigned char			*bulk_in_buffer;		/* the buffer to receive data */
-	size_t					bulk_in_size;			/* the size of the receive buffer */
-	size_t					bulk_in_filled;			/* number of bytes in the buffer */
-	size_t					bulk_in_copied;			/* already copied to user space */
-	__u8					bulk_in_endpointAddr;	/* the address of the bulk in endpoint */
-	__u8					bulk_out_endpointAddr;	/* the address of the bulk out endpoint */
-	int						errors;					/* the last request tanked */
-	bool					ongoing_read;			/* a read is going on */
-	spinlock_t				err_lock;				/* lock for errors */
-	struct kref				kref;
-	struct mutex			io_mutex;				/* synchronize I/O with disconnect */
-	wait_queue_head_t		bulk_in_wait;			/* to wait for an ongoing read */
+	struct usb_device		*udev;				/* the usb device for this device */
+	struct usb_interface		*interface;			/* the interface for this device */
+	struct semaphore		limit_sem;			/* limiting the number of writes in progress */
+
+	__u8				bulk_out_endpointAddr;		/* the address of the bulk out endpoint */
+	struct usb_anchor               submitted;
+
+	int				errors;				/* the last request tanked */
+	spinlock_t			err_lock;			/* lock for errors */
+	struct kref			kref;
+	struct mutex			io_mutex;			/* synchronize I/O with disconnect */
 };
 #define to_dkvr_dev(d) container_of(d, struct usb_dkvr, kref)
 
@@ -87,20 +73,15 @@ static void dkvr_delete(struct kref *kref)
 {
 	struct usb_dkvr *dev = to_dkvr_dev(kref);
 
-	usb_free_urb(dev->bulk_in_urb);
 	usb_put_dev(dev->udev);
-	kfree(dev->bulk_in_buffer);
 	kfree(dev);
 }
 
-/* device */
 static struct usb_dkvr_devdata usb_dkvr_data;
 
-/* led */
 static void usb_dkvr_led_brightness_set(struct led_classdev *led_dev,
 	enum led_brightness brightness);
 
-/* usb */
 static void usb_dkvr_writebyte_bulk_callback(struct urb *urb);
 static ssize_t usb_dkvr_writebyte(struct usb_dkvr *dev, unsigned char byte);
 
@@ -169,9 +150,6 @@ static void usb_dkvr_led_brightness_set(struct led_classdev *led_dev,
 	usb_dkvr_writebyte(usb_dkvr_data.usb_dev, brightness);
 }
 
-/*
- *  USB transmission
- */
 static void usb_dkvr_writebyte_bulk_callback(struct urb *urb)
 {
 	struct usb_dkvr *dev;
@@ -299,7 +277,6 @@ static int dkvr_probe(struct usb_interface *interface,
 	struct usb_dkvr *dev;
 	struct usb_host_interface *iface_desc;
 	struct usb_endpoint_descriptor *endpoint;
-	size_t buffer_size;
 	int i;
 	int retval = -ENOMEM;
 
@@ -314,36 +291,14 @@ static int dkvr_probe(struct usb_interface *interface,
 	mutex_init(&dev->io_mutex);
 	spin_lock_init(&dev->err_lock);
 	init_usb_anchor(&dev->submitted);
-	init_waitqueue_head(&dev->bulk_in_wait);
 
 	dev->udev = usb_get_dev(interface_to_usbdev(interface));
 	dev->interface = interface;
 
 	/* set up the endpoint information */
-	/* use only the first bulk-in and bulk-out endpoints */
 	iface_desc = interface->cur_altsetting;
 	for (i = 0; i < iface_desc->desc.bNumEndpoints; ++i) {
 		endpoint = &iface_desc->endpoint[i].desc;
-
-		if (!dev->bulk_in_endpointAddr &&
-		    usb_endpoint_is_bulk_in(endpoint)) {
-			/* we found a bulk in endpoint */
-			buffer_size = usb_endpoint_maxp(endpoint);
-			dev->bulk_in_size = buffer_size;
-			dev->bulk_in_endpointAddr = endpoint->bEndpointAddress;
-			dev->bulk_in_buffer = kmalloc(buffer_size, GFP_KERNEL);
-			if (!dev->bulk_in_buffer) {
-				dev_err(&interface->dev,
-					"Could not allocate bulk_in_buffer\n");
-				goto error;
-			}
-			dev->bulk_in_urb = usb_alloc_urb(0, GFP_KERNEL);
-			if (!dev->bulk_in_urb) {
-				dev_err(&interface->dev,
-					"Could not allocate bulk_in_urb\n");
-				goto error;
-			}
-		}
 
 		if (!dev->bulk_out_endpointAddr &&
 		    usb_endpoint_is_bulk_out(endpoint)) {
@@ -351,7 +306,7 @@ static int dkvr_probe(struct usb_interface *interface,
 			dev->bulk_out_endpointAddr = endpoint->bEndpointAddress;
 		}
 	}
-	if (!(dev->bulk_in_endpointAddr && dev->bulk_out_endpointAddr)) {
+	if (!dev->bulk_out_endpointAddr) {
 		dev_err(&interface->dev,
 			"Could not find both bulk-in and bulk-out endpoints\n");
 		goto error;
@@ -359,7 +314,7 @@ static int dkvr_probe(struct usb_interface *interface,
 
 	/* save our data pointer in this interface device */
 	usb_set_intfdata(interface, dev);
-	
+
 	usb_dkvr_class_register(interface);
 
 	return 0;
@@ -402,7 +357,6 @@ static void dkvr_draw_down(struct usb_dkvr *dev)
 	time = usb_wait_anchor_empty_timeout(&dev->submitted, 1000);
 	if (!time)
 		usb_kill_anchored_urbs(&dev->submitted);
-	usb_kill_urb(dev->bulk_in_urb);
 }
 
 static int dkvr_suspend(struct usb_interface *intf, pm_message_t message)
